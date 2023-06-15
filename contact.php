545f9ce2ecd54cb877f25f07e9da079d0fc0d37d
chouenne <134743005+chouenne@users.noreply.github.com>
2023-06-15 09:51:11 -0400

    update css path

--- contact.php
+++ contact.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>contact</title>
 
-    <link rel="stylesheet" href="css/contact.css">
+    <link rel="stylesheet" href="assets/css/contact.css">
 </head>
 <body>
 
